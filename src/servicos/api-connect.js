928635803e1727808c042c583dc062db442622a0
MoulOut <mmoulinlima@outlook.com>
2024-03-11 13:37:32 -0300

    feat: :sparkles: add: finalizing app

--- src/servicos/api-connect.js
+++ src/servicos/api-connect.js
@@ -1,4 +1,6 @@
-const BASE_URL = 'http://localhost:3000';
+import geraUrlPreAssinada from './geradorUrlS3';
+
+const BASE_URL = 'http://localhost:3001';
 
 function buildFetchObj(metodo, contentType, body) {
   return {
@@ -27,4 +29,26 @@ async function criaRegistro(novoRegistro) {
   }
 }
 
-export { criaRegistro };
+async function enviaArquivoViaURL(url, arquivo) {
+  const fetchObj = buildFetchObj('PUT', 'text/csv: charset=utf-8', arquivo);
+
+  try {
+    const res = await fetch(url, fetchObj);
+
+    if (res.status === 200) {
+      return 'Upload feito com sucesso.';
+    } else {
+      return 'Falha no upload.';
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+async function geraPresignURL(nomeArquivo) {
+  const urlChave = await geraUrlPreAssinada(nomeArquivo);
+
+  return urlChave;
+}
+
+export { criaRegistro, enviaArquivoViaURL, geraPresignURL };
